ee5a8861d1c2bcc16955e2b7f699c654666866cc
Brian Cordoba <briancormin@gmail.com>
2023-12-02 12:45:16 -0500

    Se agregó propiedad de localStorage al modal

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,6 @@ const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 
 
-
-//Modal pop-up
-setTimeout(function(){
-  modal.classList.toggle('active');
-}, 1200)
-
 // Button to close modal
 modalBtn.addEventListener('click', ()=>{
   modal.style.display = 'none';
@@ -35,7 +29,19 @@ right.addEventListener('mouseover', ()=>{
   audios[8].play();
 })
 
-window.addEventListener('load', ()=>{
-  audios[9].play();
-})
+
+//Modal pop-up
+modal.style.display = 'none';
+
+if(!localStorage.getItem('modal')){
+  document.addEventListener('DOMContentLoaded', ()=>{
+    audios[9].play();
+    setTimeout(function(){
+      modal.style.display = 'block';
+      modal.classList.toggle('active');
+    }, 2100)
+  })
+  localStorage.setItem('modal', 'mostrado');
+}
+
 
